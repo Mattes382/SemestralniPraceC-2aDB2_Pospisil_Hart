631ad508f318a79a02b020071634fac8924bf7b4
Mattes382 <MatejPospisill@email.cz>
2022-10-09 18:15:30 +0200

    first commit

---
+++ SemestralniPrace_Pospisil_Hart/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SemestralniPrace_Pospisil_Hart.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
